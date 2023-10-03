026e65307b7b8f6d50ac8c0f6c48f6f51b2d2599
aamoos <aamoos@naver.com>
2023-10-03 23:27:00 +0900

    css 스타일링

--- src/views/AskView.vue
+++ src/views/AskView.vue
@@ -1,16 +1,25 @@
 <template>
   <div>
-    <p v-for="item in fetchedAsk">
-      <!-- <a v-bind:href="item.url">
-        {{ item.title }}
-      </a> -->
-
-      <router-link v-bind:to="`item/${item.id}`">
-        {{ item.title }}
-      </router-link>
-      <small>{{ item.time_ago }} by {{ item.user }}</small>
-    </p>
-
+    <ul class="news-list">
+      <li v-for="item in fetchedAsk" class="post">
+        <!-- 포인트 영역 -->
+        <div class="points">
+          {{ item.points }}
+        </div>
+        <!-- 기타 정보 영역 -->
+        <div>
+          <p class="news-title">
+            <router-link v-bind:to="`item/${item.id}`">
+              {{ item.title }}
+            </router-link>
+          </p>
+          <small class="link-text">
+             {{ item.time_ago }} by
+             <router-link v-bind:to="`/user/${item.user}`" class="link-text">{{ item.user }}</router-link>
+          </small>
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
 
