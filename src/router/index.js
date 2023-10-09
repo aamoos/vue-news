d3fa2f3acfd9209b6498787c3e05ac7decfd08d2
aamoos <aamoos@naver.com>
2023-10-09 15:05:43 +0900

    HOC, Mixin

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import NewsView from '../views/NewsView.vue';
-import AskView from '../views/AskView.vue';
-import JobsView from '../views/JobsView.vue';
+import AskView from "../views/AskView.vue";
+import JobsView from "../views/JobsView.vue";
 import UserView from '../views/UserView.vue';
 import ItemView from '../views/ItemView.vue';
-import CreateListView from "../views/CreateListView";
+// import CreateListView from "../views/CreateListView";
 
 Vue.use(VueRouter);
 
@@ -20,20 +20,20 @@ export const router = new VueRouter({
       // path : url 주소
       path: "/news",
       name: "news",
-      // copmponent: NewsView,
-      component: CreateListView("NewsView"),
+      // component: CreateListView("NewsView"),
+      component: NewsView,
     },
     {
       path: "/ask",
       name: "ask",
-      // component: AskView,
-      component: CreateListView("AskView"),
+      // component: CreateListView("AskView"),
+      component: AskView,
     },
     {
       path: "/jobs",
       name: "jobs",
-      // component: JobsView,
-      component: CreateListView("JobsView"),
+      // component: CreateListView("JobsView"),
+      component: JobsView,
     },
     {
       path: "/user",
--- src/store/actions.js
+++ src/store/actions.js
@@ -1,40 +1,41 @@
 import {
-  fetchNewsList,
-  fetchJobsList,
-  fetchAskList,
+  // fetchNewsList,
+  // fetchJobsList,
+  // fetchAskList,
+  fetchList,
   fetchUserInfo,
   fetchCommentItem,
 } from "../api/index.js";
 
 export default {
-  FETCH_NEWS(context) {
-    fetchNewsList()
-      .then((response) => {
-        context.commit("SET_NEWS", response.data);
-        return response;
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  },
-  FETCH_JOBS({ commit }) {
-    fetchJobsList()
-      .then(({ data }) => {
-        commit("SET_JOBS", data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  },
-  FETCH_ASK({ commit }) {
-    fetchAskList()
-      .then(({ data }) => {
-        commit("SET_ASK", data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  },
+  // FETCH_NEWS(context) {
+  //   fetchNewsList()
+  //     .then((response) => {
+  //       context.commit("SET_NEWS", response.data);
+  //       return response;
+  //     })
+  //     .catch((error) => {
+  //       console.log(error);
+  //     });
+  // },
+  // FETCH_JOBS({ commit }) {
+  //   fetchJobsList()
+  //     .then(({ data }) => {
+  //       commit("SET_JOBS", data);
+  //     })
+  //     .catch((error) => {
+  //       console.log(error);
+  //     });
+  // },
+  // FETCH_ASK({ commit }) {
+  //   fetchAskList()
+  //     .then(({ data }) => {
+  //       commit("SET_ASK", data);
+  //     })
+  //     .catch((error) => {
+  //       console.log(error);
+  //     });
+  // },
   FETCH_USER({ commit }, name) {
     fetchUserInfo(name)
       .then(({ data }) => {
@@ -55,4 +56,9 @@ export default {
         });
     }
   },
+  FETCH_LIST({ commit }, pageName){
+    fetchList(pageName)
+    .then(({ data }) => commit('SET_LIST', data))
+    .catch(error => console.log(error));
+  }
 };
